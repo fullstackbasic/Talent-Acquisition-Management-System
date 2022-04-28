460d27a9e62d6a6a5d9cef4fd35e1a3bcca7155d
Carlos Arosemena <carlosarosemena@Carloss-MacBook-Pro-2.local>
2022-04-28 10:15:39 -0400

    Configure basic load balancing

--- career-portal-service/src/main/java/com/fullstackbasic/careerportalservice/CareerPortalServiceApplication.java
+++ career-portal-service/src/main/java/com/fullstackbasic/careerportalservice/CareerPortalServiceApplication.java
@@ -1,12 +1,29 @@
 package com.fullstackbasic.careerportalservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class CareerPortalServiceApplication {
+
+	@Autowired
+	private Environment environment;
+
+	@Value("${spring.application.name}")
+	private String serviceName;
+
+	@GetMapping("/test")
+	public String getTest(){
+		return "Test from the "+ serviceName+ " " +environment.getProperty("local.server.port");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CareerPortalServiceApplication.class, args);
